8379270edec2246bb53ec137568810f7692ab1d2
Lalo <eduardo_benji@hotmail.com>
2017-03-20 14:17:10 -0600

    dynamic routes added

--- src/app/proposal/proposal-show.component.ts
+++ src/app/proposal/proposal-show.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   moduleId: module.id,
   selector: 'app-proposal-show',
   templateUrl: './proposal-show.component.html'
 })
 
-export class ProposalShowComponent {
+export class ProposalShowComponent implements OnInit {
+  id: number;
+  routeId: any;
 
+  constructor(private route: ActivatedRoute) {
+    // Empty
+  }
+
+  ngOnInit(): void {
+    this.routeId = this.route.params.subscribe(
+      params => {
+        // By writting '+', it'll transform the string from params['id'] to a number
+        this.id = +params[ 'id' ];
+      }
+    );
+  }
 }
