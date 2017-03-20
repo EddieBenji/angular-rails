588380cfe21a11dd9a88d9d80de1e3a723b6a65e
Lalo <eduardo_benji@hotmail.com>
2017-03-20 12:16:49 -0600

    documents are listed now.

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Document } from './documents.model';
 @Component({
   moduleId: module.id,
   selector: 'app-documents',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
 })
 
 export class DocumentsComponent {
-
+  pageTitle: string = 'Document dashboard';
+  documents: Document[] = [
+    {
+      title: 'my first doc',
+      description: 'description',
+      file_url: 'google.com',
+      updated_at: 'yesterday',
+      image_url: 'google.com'
+    },
+    {
+      title: 'my second doc',
+      description: 'second description',
+      file_url: 'gmail.com',
+      updated_at: 'today',
+      image_url: 'gmail.com'
+    },
+    {
+      title: 'my last doc',
+      description: 'second description',
+      file_url: 'gmail.com',
+      updated_at: 'today',
+      image_url: 'gmail.com'
+    }
+  ];
 }
