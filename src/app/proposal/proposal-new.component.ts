d90e180ccbb891a98caa353a5b410b793360b778
Eduardo Canche <eduardo_benji@hotmail.com>
2017-04-24 18:10:24 -0500

    styles added in the form.

--- src/app/proposal/proposal-new.component.ts
+++ src/app/proposal/proposal-new.component.ts
@@ -3,7 +3,8 @@ import { Proposal } from './proposal.model';
 @Component({
   moduleId: module.id,
   selector: 'app-proposal-new',
-  templateUrl: './proposal-new.component.html'
+  templateUrl: './proposal-new.component.html',
+  styleUrls:['./proposal-new.component.css']
 })
 
 export class ProposalNewComponent {
